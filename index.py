3a014376d7dafb1fde2f2e4289bf33e8f6151681
eduardo camino <17301530@uttt.edu.mx>
2021-02-01 15:59:06 -0600

    login con conexion a db

--- index.py
+++ index.py
@@ -1,46 +1,57 @@
 from flask import (
     Flask,      render_template,        request,
-    Response,   session)
+    Response,   session,                g,
+    redirect,   url_for,                flash)
 from flask_pymongo import PyMongo
+from flask_login import LoginManager
+from werkzeug.urls import url_parse
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson import json_util
 from bson.objectid import ObjectId
-
-class usuario:
-    def __init__(self, _id, nombre, usuario, pas):
-        self._id = id
-        self.nombre = nombre
-        self.usuario = usuario
-        self.pas = pas
+from flask_login import current_user, login_user, logout_user, login_required
 
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/appone"
 mongo = PyMongo(app)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
+def star():
+    #if not session.get('logged_in'):
+    return render_template('login.html')
+
+@app.route('/login', methods=["GET" , "POST"])
 def login():
-    session.pop('user_id', None) 
-    if request.methods == 'POST':
+    #session.pop('user_id', None)
+    if request.method == 'POST':    
         username = request.form['email']
         pas = request.form['pas']
+        haspas = generate_password_hash(pas)   
         usuario = mongo.db.usuarios.find_one({'user':username})
+        r = check_password_hash(haspas, pas)
+        print (username, pas ,haspas, r)
+        print (usuario)
+        if (usuario['user'] == username and 
+            check_password_hash(usuario['pass'],pas)):
+            return render_template('main.html')
+    #   return ('oaaa')
+        return render_template('login.html')
         
     return render_template("login.html")
-
-@app.route('/registro')
-def registrar():
-    return render_template("registro.html")
 
 @app.route('/main')
 def acceder():
     return render_template("main.html")
 
+@app.route('/registro')
+def registrar():
+    return render_template("registro.html")
+
 @app.route('/newuser', methods=['POST'])
 def newuser():
-    nombre = request.json['nombre']
-    user = request.json['user']
-    pas = request.json['pass']
+    nombre = request.form['nombre']
+    user = request.form['user']
+    pas = request.form['pass']
     print(request.json)
     if  nombre and user and pas:
         hashed_pas = generate_password_hash(pas)
