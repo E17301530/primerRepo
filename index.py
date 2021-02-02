86beb85744b2bb22a0da4c4708657704a895f2e9
eduardo camino <17301530@uttt.edu.mx>
2021-02-02 13:32:09 -0600

    login con sessiones y avanvce en registro

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 from flask import (
     Flask,      render_template,        request,
     Response,   session,                g,
-    redirect,   url_for,                flash)
+    redirect,   url_for,                flash,
+    abort)
 from flask_pymongo import PyMongo
 from flask_login import LoginManager
 from werkzeug.urls import url_parse
@@ -12,8 +13,17 @@ from flask_login import current_user, login_user, logout_user, login_required
 
 
 app = Flask(__name__)
+app.secret_key = 'somesecretkeythatonlyishouldknow'
 app.config["MONGO_URI"] = "mongodb://localhost:27017/appone"
 mongo = PyMongo(app)
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        usuario = mongo.db.usuarios.find_one({'user':session['user_id']})
+        g.usuario = usuario
+        print(g.usuario)
 
 @app.route('/')
 def star():
@@ -22,25 +32,25 @@ def star():
 
 @app.route('/login', methods=["GET" , "POST"])
 def login():
-    #session.pop('user_id', None)
-    if request.method == 'POST':    
+   
+    if request.method == 'POST': 
+        session.pop('user_id', None) 
+        session.clear()  
         username = request.form['email']
-        pas = request.form['pas']
-        haspas = generate_password_hash(pas)   
+        pas = request.form['pas']  
         usuario = mongo.db.usuarios.find_one({'user':username})
-        r = check_password_hash(haspas, pas)
-        print (username, pas ,haspas, r)
-        print (usuario)
         if (usuario['user'] == username and 
             check_password_hash(usuario['pass'],pas)):
-            return render_template('main.html')
-    #   return ('oaaa')
-        return render_template('login.html')
+            session['user_id'] = usuario['user']
+            return redirect(url_for('main'))
+        return redirect(url_for('login'))
         
     return render_template("login.html")
 
 @app.route('/main')
-def acceder():
+def main():
+    if not g.usuario:
+        return render_template("login.html")
     return render_template("main.html")
 
 @app.route('/registro')
@@ -50,14 +60,17 @@ def registrar():
 @app.route('/newuser', methods=['POST'])
 def newuser():
     nombre = request.form['nombre']
+    apellidoP = request.form['apellidoPaterno']
+    apellidoM = request.form['ApellidoMaterno']
+    tipoUsuario = request.form['tipoUusuario']
     user = request.form['user']
     pas = request.form['pass']
-    print(request.json)
-    if  nombre and user and pas:
+    if  nombre and apellidoP and apellidoM and tipoUsuario and user and pas:
         hashed_pas = generate_password_hash(pas)
-        print(hashed_pas)
         id = mongo.db.usuarios.insert(
-            {'nombre': nombre, 'user': user, 'pass': hashed_pas}
+            {'nombre': nombre, 'apellidoPaterno':apellidoP,
+             'apellidoMaterno': apellidoM,'tipoUsuario': tipoUsuario,
+             'user': user, 'pass': hashed_pas}
         )
         respo = {
             "id": str(id),
